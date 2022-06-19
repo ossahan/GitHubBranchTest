7b344ac48033c6933bba807b014bb950458eb167
Osman Sahan <100969747+ossahan@users.noreply.github.com>
2022-06-19 17:52:35 +0300

    2.degisiklik

--- .idea/src/GithubBranch.java
+++ .idea/src/GithubBranch.java
@@ -1,4 +1,7 @@
 package src;
 
 public class GithubBranch {
+    public static void main(String[] args) {
+        System.out.println("ortak classta degisiklik yaptim");
+    }
 }
--- .idea/src/ozellik.java
+++ .idea/src/ozellik.java
@@ -1,4 +1,8 @@
 package src;
 
 public class ozellik {
+    public static void main(String[] args) {
+        System.out.println(" kendi classimda degisiklik");
+
+    }
 }
